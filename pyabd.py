6a9c137260c7f0a3d08493252661522553241357
Celestial <116771638+celestial-official@users.noreply.github.com>
2024-02-13 16:29:45 +0100

    Updated pyabd.py

--- pyabd.py
+++ pyabd.py
@@ -11,11 +11,9 @@ class ADBTool:
         adbCommand = "where" if (os.name == "nt") else "which"
 
         try:
-            output = subprocess.check_output([adbCommand, "adb"]).decode().strip()
+            output = subprocess.check_output([adbCommand, "adb"], stderr=subprocess.DEVNULL).decode().strip()
             return output if (output) else None
         except subprocess.CalledProcessError:
-            print("(!) Couldn't check for ADB.")
-            time.sleep(1.5)
             return None
         
     def clearScreen(self):
@@ -46,6 +44,23 @@ class ADBTool:
 class Device:
     def __init__(self, serial):
         self.serial = serial
+        self.manufacturer = self.getDeviceInfos()
+
+    def getDeviceInfos(self):
+        try:
+            output = subprocess.check_output(["adb", "-s", self.serial, "shell", "getprop", "ro.product.manufacturer"], stderr=subprocess.DEVNULL).decode("utf-8").strip()
+            manufacturer = output if (output) else "Unknown"
+        except subprocess.CalledProcessError:
+            manufacturer = "Unknown"
+        
+        """
+        try:
+            output = subprocess.check_output(["adb", "-s", self.serial, "shell", "getprop", "ro.product.model"], stderr=subprocess.DEVNULL).decode("utf-8").strip()
+            model = output if (output) else "Unknown Model"
+        except subprocess.CalledProcessError:
+            model = "Unknown"
+        """
+        return manufacturer
     
     def isRooted(self):
         print("(+) Checking for ROOT access...")
@@ -65,6 +80,19 @@ class Device:
 
         subprocess.run(["adb", "-s", self.serial, "push", localPath, devicePath], check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     
+    def copyClipboardToFile(self, localPath):
+        print("(+) Copying clipboard content to file...")
+        time.sleep(1.5)
+        adbTool.clearScreen()
+
+        if (self.isRooted()):
+            clipboardContent = subprocess.check_output(["adb", "-s", self.serial, "shell", "content", "query", "--uri", "content://clipboard/clip"]).decode("utf-8")
+            clipboardContent = clipboardContent.strip()
+        else:
+            adbTool.clearScreen()
+            print("(!) This fearures is coming soon. As right now, only rooted phones can use it.")
+            time.sleep(1.5)
+
     def executeCommand(self, command):
         subprocess.run(["adb", "-s", self.serial, "shell", command], check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
@@ -86,15 +114,46 @@ if (__name__ == "__main__"):
     
     print("[AVAILABLE DEVICES]:")
     for i, deviceSerial in enumerate(devices):
-        print(f"[{i+1}]: {deviceSerial}")
+        device = Device(deviceSerial)
+        print(f"[{i+1}]: {device.manufacturer} | {device.serial}")
     
-    choice = input("(?) Enter the number of the device you want to use (default is 1): ")
+    choice = input("\n(?) Enter the number of the device you want to use (default is 1): ")
     choice = int(choice) if (choice.isdigit() and 1 <= int(choice) <= len(devices)) else 1
     selectedDevices = Device(devices[choice - 1])
     adbTool.clearScreen()
 
-    filePath = input("(?) Enter the file path to display (default is ./content.txt): ")
-    devicePath = "/mnt/sdcard/content.txt" if (selectedDevices.isRooted()) else "/data/local/tmp/content.txt"
+    while (True):
+        adbTool.clearScreen()
+        print("[TRANSFER OPTIONS]")
+        print("[1] PC to Phone")
+        print("[2] Phone to PC")
 
-    selectedDevices.pushFile(filePath, devicePath)
-    selectedDevices.executeCommand(f"am start -a android.intent.action.VIEW -d file://{devicePath} -t text/plain")
+        choice = input("\n(?) Select a valid option: ")
+        adbTool.clearScreen()
+
+        if (choice.lower() in ["1", "pc to phone", "pc"]):
+            filePath = input("(?) Enter the file path to display (text/plain): ")
+            adbTool.clearScreen()
+            devicePath = "/mnt/sdcard/content.txt" if (selectedDevices.isRooted()) else "/data/local/tmp/content.txt"
+
+            try:
+                selectedDevices.pushFile(filePath, devicePath)
+                selectedDevices.executeCommand(f"am start -a android.intent.action.VIEW -d file://{devicePath} -t text/plain")
+            except Exception:
+                adbTool.clearScreen()
+                print(f"(!) Couldn't transfer file '{filePath}' to device '{selectedDevices.serial}'")
+        elif (choice.lower() in ["2", "phone to pc", "phone"]):
+            localPath = input("(?) Enter the local path on PC to save the clipboard: ")
+            adbTool.clearScreen()
+
+            try:
+                selectedDevices.copyClipboardToFile(localPath)
+            except subprocess.CalledProcessError as error:
+                adbTool.clearScreen()
+                print(f"(!) Couldn't copy clipboard content to '{localPath}'.")
+                print(error)
+                time.sleep(1000)
+        else:
+            adbTool.clearScreen()
+            print("(!) Invalid option has been choosed.")
+            time.sleep(1.5)
